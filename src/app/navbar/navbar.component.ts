3c4b8fe1527aafeab32adc1aea4da7f63d103277
nyount14 <90732548+nyount14@users.noreply.github.com>
2021-12-13 21:02:43 -0600

    nav bar drop down

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  isShown:boolean = false;
   constructor() { }
 
   ngOnInit(): void {
