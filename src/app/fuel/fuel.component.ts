8dc04ef1d486613805c307af4ed6a635e2aa40d2
nyount14 <90732548+nyount14@users.noreply.github.com>
2021-12-27 19:52:08 -0600

    update transaction request
    switched from oninit to it's own request button

--- src/app/fuel/fuel.component.ts
+++ src/app/fuel/fuel.component.ts
@@ -13,46 +13,56 @@ export class FuelComponent implements OnInit {
   balance = 0;
   amount;
   loadedTransactions: Transaction[] = [];
+  transactionType = '';
 
   constructor(private http: HttpClient, private authService: AuthService) {}
 
   ngOnInit() {
 
-      return this.http
-      .get<Transaction>('https://money-manager-9ab10-default-rtdb.firebaseio.com/posts.json',
-    )
-    .pipe(map(responseData => {
-      const postsArray: Transaction[] = [];
-        for (const key in responseData ) {
-          if (responseData.hasOwnProperty(key))
-          postsArray.push({ ...responseData[key], id: key })
-        }
-        return postsArray;
-      }))
-      .subscribe(posts => {
-        this.loadedTransactions = posts;
-      });
   }
 
   onCreatePost(postData) {
-    console.log(postData)
-    // this.balance = this.balance + parseInt(this.amount);
+    console.log(postData);
     this.http
       .post<{ name: string }>(
         'https://money-manager-9ab10-default-rtdb.firebaseio.com/posts.json',
         postData
       )
       .subscribe((responseData) => {
-        console.log(responseData);
+        console.log(this.amount);
+        if (postData.transactionType == 'credit') {
+          this.balance = this.balance + parseInt(postData.amount);
+        } else {
+          this.balance = this.balance - parseInt(postData.amount);
+        }
+        console.log(this.balance);
       });
   }
 
-  onDebit() {
-    this.balance = this.balance - this.amount;
-  }
+  // onDebit() {
+  //   this.balance = this.balance - this.amount;
+  // }
 
-  onUpdate(amount) {
-    this.amount = amount.target.value;
+  // onUpdate(amount) {
+  //   this.amount = amount.target.value;
+  // }
+  onReturn(){
+    return this.http
+      .get<Transaction>(
+        'https://money-manager-9ab10-default-rtdb.firebaseio.com/posts.json'
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray: Transaction[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key))
+              postsArray.push({ ...responseData[key], id: key });
+          }
+          return postsArray;
+        })
+      )
+      .subscribe((posts) => {
+        this.loadedTransactions = posts;
+      });
   }
 }
-
